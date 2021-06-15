1ff1ab4815a2a28de733c460ebf11783c8ee24d9
rita8216 <69557395+rita8216@users.noreply.github.com>
2021-06-16 00:47:15 +0800

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,9 @@
+$(window).scroll(function(){
+  if($(window).scrollTop()>0)
+    $(".navbar").removeClass("transparent")
+  else
+    $(".navbar").addClass("transparent")
+  
+});
+
+var s = skrollr.init();
